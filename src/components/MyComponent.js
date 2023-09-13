f40ca3f344be9a4019997bcb93632713f5d675b9
HoangTruong3843 <hoang.truong@ucdenver.edu>
2023-09-13 00:58:28 -0500

    commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component  --->>> this course will start with class component only
+// function component
+import React from 'react'
+class MyComponent extends React.Component {
+    // cu phap jsx, only react has this, cho phep viet code javascript in a html
+    render(){
+        return (
+            <div>
+                My first component
+            </div>
+        );
+    }
+}
+
+// muon su dung cai .js nay phai import no
+
+export default MyComponent;
