5cc82f6d3587a5e2a9616e1ed9bd12d7bf43fa64
HoangTruong3843 <hoang.truong@ucdenver.edu>
2023-09-25 12:20:23 -0500

    video 30 added

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class DisplayInfor extends React.Component{
+    handleShowHide=()=>{
+         this.setState({
+            isShowListUser : !this.state.isShowListUser
+         })
+     }
+    
+    state={
+        isShowListUser:true }
+    
     render(){
         // ở file MyComponents.js (cha) chúng ta đã gán name và age vào cho component 
         // DisplayInfor.js (con), nên chúng ta có thể gọi name và age = cách úsing
@@ -11,18 +20,31 @@ class DisplayInfor extends React.Component{
 
         // sử dụng map để truyền dữ liệu từ 1 array (listUsers) được gán giá trị từ file MyComponent.js
         const {listUsers} = this.props;
-        console.log(this.props)
-        return(<div>
-            {listUsers.map((user)=>{
-                //tiếp theo nếu cần phải đặt cho từng user một key 
-                return(<div key={user.id}>
-                    <div>My name 's {user.name}</div>
-                    <div>My age 's {user.age}</div>
-                    <hr/>
-                </div>)
-            })}
-            
-        </div>)
+       // console.log(this.props)
+        console.table(listUsers)
+        
+        return(
+            <div>
+                <div> 
+                    <span onClick = {()=>{this.handleShowHide()}}>
+                        {this.state.isShowListUser === true? <button>Hide users list</button> :<button>Show users list</button>}
+                    </span> 
+                </div> 
+                {this.state.isShowListUser && <div>
+                    {listUsers.map((user)=>{
+                        //tiếp theo nếu cần phải đặt cho từng user một key
+                        return(
+                                //cau dieukien if
+                                <div key={user.id} className = {+user.age > 18? "green":"red"}>
+                                    <div>My name 's {user.name}</div>
+                                    <div>My age 's {user.age}</div>
+                                    <hr/>
+                                </div>)               
+                    })} 
+                </div> }
+                
+                
+            </div>)
     }
 }
     
